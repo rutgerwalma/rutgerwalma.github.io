51e60dd99cd2ca0d7a8c138411b032772eea682c
Rutger Walma <rutgerwalma@live.nl>
2020-04-11 16:27:31 +0200

    budgettingtool new version

--- budgettingtool/js/scripts.js
+++ budgettingtool/js/scripts.js
@@ -1,34 +1,40 @@
 
-
 function calculate(){
+    var formatter = new Intl.NumberFormat('nl-NL', {
+        style: 'currency',
+        currency: 'EUR',
+        });
+
     var totalIncome = 0.00;
+
     $("#income-table .rightinput").each(function() {
         if(this.value){
-            totalIncome = totalIncome + parseFloat(this.value);
+            let value = parseFloat(this.value.toString().replace(",", "."))
+            totalIncome = totalIncome +value;
         }
     });
-    console.log('income: ' + totalIncome);
 
     var totalExpenses = 0.00;
     $("#expenses-table .rightinput").each(function() {
         if(this.value){
-            totalExpenses = totalExpenses + parseFloat(this.value);
+            let value = parseFloat(this.value.toString().replace(",", "."))
+            totalExpenses = totalExpenses +value;
         }
     });
-    console.log('expenses: ' + totalExpenses);
 
     var balance = 0.00
-    balance = totalIncome - totalExpenses
 
-    balance = numeral(balance).format('0,0.00');
+    balance = totalIncome - totalExpenses
+    balance =  formatter.format(balance);
 
     $('#balance').text(balance);
 
-    if(balance < 0){
-        $('.saldo').css('box-shadow','10px 6px 40px 0px rgba(255, 22, 22, 0.57)')
-    }else{
-        $('.saldo').css('box-shadow','10px 10px 74px 2px rgba(0, 155, 46, 0.38)')
-    }
+    // if(balance > 0.00){
+    //     $('.saldo').css('box-shadow','10px 6px 40px 0px rgba(255, 22, 22, 0.57)')
+    // }else{
+    //     $('.saldo').css('box-shadow','10px 10px 74px 2px rgba(0, 155, 46, 0.38)')
+
+    // }
 }
 
 function addTableRow(button){
@@ -44,6 +50,7 @@ function addTableRow(button){
     calculate();
   });
 
+
 }
 
 function deleteTableRow(button){
@@ -51,6 +58,8 @@ function deleteTableRow(button){
 }
 
 $( document ).ready(function() {
+    numbro.setLanguage('nl-NL');
+
     $('#date').text(moment().format('MMMM YYYY'));
 
     $(".rightinput").change(function () {
@@ -58,3 +67,28 @@ $( document ).ready(function() {
     });  
 
 });
+
+function checkKeyCode(e) {
+    var $this = $(this);
+    if ((event.which != 44 || $this.val().indexOf(',') != -1) &&
+       ((event.which < 48 || event.which > 57) &&
+       (event.which != 0 && event.which != 8))) {
+           event.preventDefault();
+    }
+
+    var text = $(this).val();
+    if ((event.which == 44) && (text.indexOf(',') == -1)) {
+        setTimeout(function() {
+            if ($this.val().substring($this.val().indexOf(',')).length > 3) {
+                $this.val($this.val().substring(0, $this.val().indexOf(',') + 3));
+            }
+        }, 1);
+    }
+
+    if ((text.indexOf(',') != -1) &&
+        (text.substring(text.indexOf(',')).length > 2) &&
+        (event.which != 0 && event.which != 8) &&
+        ($(this)[0].selectionStart >= text.length - 2)) {
+            event.preventDefault();
+    }     
+};
